0b39131281720a97d14bb6e044f3a00f2d472af3
ElimuStuart <stuartelimu@gmail.com>
2019-11-21 22:27:37 +0300

    edit gitignore file

--- audio/views.py
+++ audio/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse, Message
 import youtube_dl
 import os
 
@@ -33,10 +33,17 @@ def get_song_details(url):
 
 @csrf_exempt
 def webhook(request):
-    req = json.loads(request.body)
-    video_url = req.get('queryResult').get('parameters')['url']
-    song = get_song_details(video_url)
-    print(song)
-    fullfillmentText = {'fulfillmentText': song}
-    return JsonResponse(fullfillmentText, safe=False)
+    req = request.body
+    # video_url = req.get('queryResult').get('parameters')['url']
+    # song = get_song_details(video_url)
+    # print(song)
+    # fullfillmentText = {'fulfillmentText': song}
+    response = MessagingResponse()
+
+    msg = response.message("hello")
+    img = "https://images.unsplash.com/photo-1573258759625-f40df05cfee3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+    msg.media(img)
+    print(req)
+
+    return HttpResponse(response)
 
