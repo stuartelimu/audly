d9854f8d3a4cc7ab2dbf307f0c163879abd2cd89
ElimuStuart <stuartelimu@gmail.com>
2019-11-23 23:44:01 +0300

    add s3 settings

--- audly/settings.py
+++ audly/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'audio',
+
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -130,4 +132,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 TWILIO_ACCOUNT_SID = config('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = config('TWILIO_AUTH_TOKEN')
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
